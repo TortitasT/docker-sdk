bce4c6479283f215691662a1154e59c30e5c8d8c
TortitasT <TortitasT@users.noreply.github.com>
2023-02-11 19:23:13 +0000

    Commit from GitHub Actions (Fix Styles)

--- src/versions/Version.php
+++ src/versions/Version.php
@@ -6,13 +6,21 @@ use Tortitas\DockerSDK\Container;
 
 interface Version
 {
-  public static function version(): array;
-  public static function up(string $path): void;
-  public static function down(string $path): void;
-  public static function pull(string $image): void;
-  public static function list(): array;
-  public static function create(Container $container): Container;
-  public static function start(Container $container): Container;
-  public static function stop(Container $container): Container;
-  public static function remove(Container $container): void;
+    public static function version(): array;
+
+    public static function up(string $path): void;
+
+    public static function down(string $path): void;
+
+    public static function pull(string $image): void;
+
+    public static function list(): array;
+
+    public static function create(Container $container): Container;
+
+    public static function start(Container $container): Container;
+
+    public static function stop(Container $container): Container;
+
+    public static function remove(Container $container): void;
 }
